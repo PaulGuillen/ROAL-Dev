d93c5e6dadcf1b6a538f543eab6e916bd50c0746
Paul Guillen <paulguillen190@gmail.com>
2023-01-28 14:59:51 -0500

    Creating validations to consult a worker

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,6 +1,4 @@
 
-Å
-Qapp/src/main/java/com/example/roal/view/management_worker/CreateWorkerActivity.kt,1\b\1ba10fd57d69351a1a4ebf8bef34c8d598de8d90
 i
 9app/src/main/java/com/example/roal/models/ResponseHttp.kt,d\3\d32cc0ee87f78835952fc48fec9f0b0d71196871
 d
--- app/src/main/java/com/example/roal/view/emergency/EmergencyActivity.kt
+++ app/src/main/java/com/example/roal/view/emergency/EmergencyActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.content.ContextCompat
 import com.example.roal.R
 import com.example.roal.databinding.ActivityEmergencyBinding
@@ -25,5 +26,36 @@ class EmergencyActivity : AppCompatActivity() {
         binding.include.toolbar.title = "Emergencia"
         setSupportActionBar(binding.include.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.btnSearchByDNI.setOnClickListener { searchingByDNI() }
+        binding.btnSearchByPhoto.setOnClickListener { searchingByPhoto() }
+        binding.btnSearchByQR.setOnClickListener { searchingByQR() }
     }
+
+    private fun searchingByDNI() {
+        binding.searchBox.visibility = View.VISIBLE
+        binding.imagePhoto.visibility = View.GONE
+        binding.imageQR.visibility = View.GONE
+        /**Si el DNI existe se muestra*/
+      // binding.linearDNI.visibility = View.VISIBLE
+        binding.btnSearch.visibility = View.VISIBLE
+    }
+
+    private fun searchingByPhoto() {
+        binding.searchBox.visibility = View.GONE
+        binding.imagePhoto.visibility = View.VISIBLE
+        binding.imageQR.visibility = View.GONE
+        /**Si el DNI existe se muestra*/
+      //  binding.linearDNI.visibility = View.VISIBLE
+        binding.btnSearch.visibility = View.VISIBLE
+    }
+
+    private fun searchingByQR() {
+        binding.searchBox.visibility = View.GONE
+        binding.imagePhoto.visibility = View.GONE
+        binding.imageQR.visibility = View.VISIBLE
+        /**Si el DNI existe se muestra*/
+        //  binding.linearDNI.visibility = View.VISIBLE
+        binding.btnSearch.visibility = View.VISIBLE
+    }
+
 }
